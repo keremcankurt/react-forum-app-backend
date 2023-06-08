efb0b37376b907a83247073f81fd225b9a8bce23
keremcankurt <keremkurt278@gmail.com>
2023-06-08 16:09:01 +0300

    ikinci gönderme

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDatabase();
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Credentials", "true");
