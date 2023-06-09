b9f5820e41b266f524a0c753f3f3b9213371fc52
keremcankurt <127202679+keremcankurt@users.noreply.github.com>
2023-06-09 20:20:12 +0300

    Update tokenHelpers.js

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -3,7 +3,7 @@ const sendJwtToClient = (user, res) => {
     const { JWT_COOKIE, NODE_ENV } = process.env;
     return res
       .cookie("access_token", token, {
-        secure: true,
+        secure: false,
         expires: new Date(Date.now() + parseInt(JWT_COOKIE) * 1000),
       })
       .status(200)
