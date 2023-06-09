e2748d29ae1f55ca5183c4086ad2e7660ada359d
keremcankurt <127202679+keremcankurt@users.noreply.github.com>
2023-06-09 20:30:46 +0300

    Update tokenHelpers.js

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -1,12 +1,13 @@
 const sendJwtToClient = (user, res) => {
     const token = user.generateJwtFromUser();
     const { JWT_COOKIE, NODE_ENV } = process.env;
-    return res
-      .cookie("access_token", token, {
-        secure: false,
-        samSite: "None",
-        expires: new Date(Date.now() + parseInt(JWT_COOKIE) * 1000),
-      })
+    return res.cookie("access_token", token, {
+      httpOnly: true,
+      secure: true, // "SameSite=None" kullanırken bu değerin true olarak ayarlı olduğundan emin olun
+      sameSite: "None", // Çapraz site kullanımı için SameSite değerini None olarak ayarlayın
+      expires: new Date(Date.now() + parseInt(JWT_COOKIE) * 1000),
+    });
+
       .status(200)
       .json({
         fullName: user.name + " " + user.surname,
